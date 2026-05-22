1cf4667c71f634b070396d24d6ad939a322fc962
ManhNang <manhnang12d@gmail.com>
2026-05-22 15:38:45 +0700

    add all file

---
+++ Sử dụng các hàm có sẵn/script.js
@@ -0,0 +1,8 @@
+//Bài 1: Nối các phần tử trong mảng thành chuỗi
+const myColor = ["Red", "Green", "White", "Black"];
+let result = myColor.join(", ");
+console.log(result);
+
+//Bài 2 Chèn dấu "-" giữa các số chẵn
+let rawString = prompt("Nhập 1 chuỗi các số: ");
+console.log(rawString.split(""));
